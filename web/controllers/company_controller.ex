d5b3c638611ce7f489310118a4e1711089342f7d
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2019-01-10 12:45:54 +0100

    Added company phoenix model

--- web/controllers/company_controller.ex
+++ web/controllers/company_controller.ex
@@ -3,19 +3,19 @@ defmodule ElixirNinetyNineBackendChallenge.CompanyController do
 
   def index(conn, _params) do
     companies = [
-      %{
+      %ElixirNinetyNineBackendChallenge.Company{
         id: 1,
         name: "Apple Inc.",
         ric: "APPL",
         sharePrice: 223.154
       },
-      %{
+      %ElixirNinetyNineBackendChallenge.Company{
         id: 2,
         name: "Microsoft Corporation",
         ric: "MSFT",
         sharePrice: 102.574
       },
-      %{
+      %ElixirNinetyNineBackendChallenge.Company{
         id: 3,
         name: "Alphabet Inc.",
         ric: "GOOG",
---
+++ web/models/company.ex
@@ -0,0 +1,4 @@
+# web/models/company.ex
+defmodule ElixirNinetyNineBackendChallenge.Company do
+    defstruct [:id, :name, :ric, :sharePrice]
+end
